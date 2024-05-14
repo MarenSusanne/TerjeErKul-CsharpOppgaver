994a832ba2f9425e91dc3501413400fb2fff0df7
MarenSusanne <Marensut@getacademy.no>
2024-05-14 13:02:01 +0200

    Add project files.

---
+++ TerjeErKul/Program.cs
@@ -0,0 +1,25 @@
+﻿for (var index = 0; index < 5; index++)
+{
+    Console.WriteLine("Terje er kul!");
+}
+
+string argus = "Fiddelibom";
+foreach (char arg in argus)
+{
+    Console.WriteLine(arg);
+}
+
+bool condition = true;
+int i = 1;
+while (condition)
+{
+    if (i < 10)
+    {
+        Console.WriteLine($"Runde nr {i}");
+        i++;
+    }
+    else
+    {
+        condition = false;
+    }
+}
